07bef4773bd1fe003256fbff0ce2dcbac908764e
Atul Sharma <attulsharmma@gmail.com>
2024-12-02 01:07:58 +0530

    feat(token) replace with bearer token

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { messages } from "../constants.js";
 const { JWT_SECRET } = process.env;
 export const verifyToken = (req, res, next) => {
-  const authToken = req.headers.token;
+  const authToken = req.headers.authorization.split(" ")[1]; // Extract token from Bearer Token
   if (authToken) {
     jwt.verify(authToken, JWT_SECRET, (err, user) => {
       if (err) {
