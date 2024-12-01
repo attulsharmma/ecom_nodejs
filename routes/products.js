4dec811733f6eca7dcc7b86ace21b9039903bdd1
Atul Sharma <attulsharmma@gmail.com>
2024-12-02 01:08:33 +0530

    refactor(products) removed unnecessary comments

--- routes/products.js
+++ routes/products.js
@@ -10,15 +10,9 @@ import { verifyToken } from "../middlewares/verifyToken.js";
 
 const productRouter = Router();
 
-//Get All Products
 productRouter.get("/getAllProducts", verifyToken, getAllProducts);
-//Get Single Product
 productRouter.get("/getProductById", verifyToken, getSingleProduct);
-//Create Product
 productRouter.post("/createProduct", verifyToken, createNewProduct);
-//Update Product
 productRouter.put("/updateProduct", verifyToken, updateProduct);
-//Delete Product
 productRouter.delete("/deleteProduct", verifyToken, deleteProduct);
-
 export default productRouter;
